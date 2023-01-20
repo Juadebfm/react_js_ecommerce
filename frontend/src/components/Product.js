0482a0896e86fc4b6c2776f9c7daa23fd77c05c6
Julius Adebowale <juadebgabriel@gmail.com>
2023-01-19 18:47:55 -0600

    Friday's push

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useContext } from "react";
+//import link
+import { Link } from "react-router-dom";
+//import icons
+import { BsPlus, BsEyeFill } from "react-icons/bs";
 
-const Product = () => {
-  return <div>Product</div>;
+const Product = ({ product }) => {
+  console.log(product);
+  //destructure product
+  const { id, image, category, title, price } = product;
+  return (
+    <div>
+      <div className="border border-[#e4e4e4] h-[300px] mb-4 relative overflow-hidden group transition">
+        <div className="w-full h-full flex justify-center items-center">
+          {/* image */}
+          <div className="w-[200px] mx-auto flexer">
+            <img
+              className="max-h-[160px] group-hover:scale-110 transition duration-300"
+              src={image}
+              alt=""
+            />
+          </div>
+          {/* buttons */}
+          <div className="absolute top-0 -right-16 group-hover:right-2 group-hover:top-2 bg-red-500/5 p-2 flex-col items-center justify-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300">
+            <button>
+              <div className="flex justify-center items-center text-white w-10 h-10 bg-red-500">
+                <BsPlus className="text-3xl" />
+              </div>
+            </button>
+            <Link
+              to={`/product/${id}`}
+              className="w-10 h-10 mt-1 bg-white flexer text-primary drop-shadow-xl"
+            >
+              <BsEyeFill />
+            </Link>
+          </div>
+        </div>
+        <div>2</div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 //import product context
 import { ProductContext } from "../contexts/ProductContext";
+//import components
+import Product from "../components/Product";
 
 const Home = () => {
   //get product from product context
@@ -19,9 +21,10 @@ const Home = () => {
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0">
             {filteredProducts.map((product) => {
               return (
-                <div className="w-full h-[300px] bg-pink-200" key={product.id}>
-                  {product.title}
-                </div>
+                // <div className="w-full h-[300px] bg-pink-200" key={product.id}>
+                //   {product.title}
+                // </div>
+                <Product product={product} key={product.id} />
               );
             })}
           </div>
